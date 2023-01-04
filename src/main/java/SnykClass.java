c27a781cea3d39ca7b81ef3fe23b1b09a7f6d2f2
GAGAN SURI <gagan.suri@gmail.com>
2023-01-04 17:40:37 -0600

    Update SnykClass.java

--- src/main/java/SnykClass.java
+++ src/main/java/SnykClass.java
@@ -5,14 +5,14 @@ public class SnykClass {
 
 public void login() {
   String userName = "gagan";
-  String pwd = "testpassword";
+  String p = "testpassword";
   boolean loggedIn = authenticate(userName,password);
   System.out.println("logged in successfully");
 
 }
 
-public boolean authenticate(String u,String pwd) {
-   return user.equals("gagan") && pwd.equals("testpassword");
+public boolean authenticate(String u,String p) {
+   return user.equals("gagan") && p.equals("testpassword");
 
 }
 
